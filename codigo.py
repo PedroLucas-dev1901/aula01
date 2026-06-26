33e9f9e76e96f4224c93da85dd67c801b98b200a
Pedro Lucas <sousa.pontes1901@gmail.com>
2026-06-26 19:17:02 -0300

    Create codigo.py

---
+++ codigo.py
@@ -0,0 +1,25 @@
+repetir = "S"
+
+while repetir == "S":
+
+    nomeCliente = input("Nome do cliente: ")
+
+    valorCompra = float(input("Valor da compra: R$ "))
+
+    if valorCompra > 100:
+
+        desconto = valorCompra * 0.10
+
+    else:
+
+        desconto = 0
+
+    valorFinal = valorCompra - desconto
+
+    print("\nCliente:", nomeCliente)
+    print("Desconto: R$", desconto)
+    print("Valor final: R$", valorFinal)
+
+    repetir = input("\nDeseja realizar outro atendimento? (S/N): ").upper()
+
+print("Programa encerrado.")
